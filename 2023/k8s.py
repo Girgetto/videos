6df5be617c18f6ae31a5a68640f741c2d2d6a2e3
Giorgio Grassini <giorgiograssini@gmail.com>
2024-01-10 00:14:03 +0100

    Refactor Kubernetes node and add control plane components

--- 2023/k8s.py
+++ 2023/k8s.py
@@ -92,11 +92,11 @@ class K8sNode(Scene):
         k8s_node.scale(2.5)
         k8s_node_label = Text("Node", font_size=36)
         k8s_node_label.next_to(k8s_node, UP)
-        k8s_node_group = VGroup(k8s_node, k8s_node_label)
-        self.play(Create(k8s_node_group))
         # Add kubelet label
         kubelet_label = Text("Kubelet", font_size=24)
-        kubelet_label.next_to(k8s_node, DOWN, buff=0.1)
+        kubelet_label.next_to(k8s_node, DOWN, buff=-0.5)
+        k8s_node_group = VGroup(k8s_node, k8s_node_label, kubelet_label)
+        self.play(Create(k8s_node_group))
 
         # Animate the Kubernetes pod moving into the Kubernetes node
         self.play(pod_and_containers.animate.move_to(k8s_node.get_center()))
@@ -149,6 +149,84 @@ class K8sNode(Scene):
         k8s_cluster_label.next_to(k8s_cluster, UP)
         k8s_cluster_group = VGroup(k8s_cluster, k8s_cluster_label)
         self.play(Create(k8s_cluster_group))
+        # Move on the right
+        self.play(k8s_cluster_group.animate.scale(4))
+
+class ControlPlane(Scene):
+    def construct(self):
+        # Create a vertical rectangle on the left side
+        rectangle = Rectangle(height=8, width=4, color=WHITE)
+        rectangle.shift(LEFT*5)
+        self.play(Create(rectangle))
+        self.wait(2)
+        # control plane kube-api-server component
+        rectangle1 = Rectangle(height=1, width=3, color=WHITE)
+        rectangle1.move_to(rectangle.get_center())
+        self.play(Create(rectangle1))
+        # add text inside the rectangle
+        rectangle_label = Text("cloud-control-manager", font_size=18)
+        rectangle_label.move_to(rectangle.get_center())
+        self.play(Write(rectangle_label))
+        self.wait(2)
+        # create group with rectangle and text
+        rectangle_group = VGroup(rectangle1, rectangle_label)
+        # animate the rectangle moving down
+        self.play(rectangle_group.animate.shift(DOWN * 2))
+        self.wait(2)
+
+        # control plane etcd component
+        rectangle2 = Rectangle(height=1, width=3, color=WHITE)
+        rectangle2.move_to(rectangle.get_center())
+        self.play(Create(rectangle2))
+        # add text inside the rectangle
+        rectangle_label2 = Text("etcd", font_size=24)
+        rectangle_label2.move_to(rectangle.get_center())
+        self.play(Write(rectangle_label2))
+        self.wait(2)
+        # create group with rectangle and text
+        rectangle_group2 = VGroup(rectangle2, rectangle_label2)
+        # animate the rectangle moving down
+        self.play(rectangle_group2.animate.shift(DOWN * 1))
+        self.wait(2)
+
+        # control plane kube-scheduler component
+        rectangle3 = Rectangle(height=1, width=3, color=WHITE)
+        rectangle3.move_to(rectangle.get_center())
+        self.play(Create(rectangle3))
+        # add text inside the rectangle
+        rectangle_label3 = Text("kube-scheduler", font_size=20)
+        rectangle_label3.move_to(rectangle.get_center())
+        self.play(Write(rectangle_label3))
+        self.wait(2)
+        # create group with rectangle and text
+        rectangle_group3 = VGroup(rectangle3, rectangle_label3)
+        self.play(rectangle_group3.animate.shift(UP * 1))
+        self.wait(2)
+
+        # control plane kube-controller-manager component
+        rectangle4 = Rectangle(height=1, width=3, color=WHITE)
+        rectangle4.move_to(rectangle.get_center())
+        self.play(Create(rectangle4))
+        # add text inside the rectangle
+        rectangle_label4 = Text("kube-controller-manager", font_size=16)
+        rectangle_label4.move_to(rectangle.get_center())
+        self.play(Write(rectangle_label4))
+        self.wait(2)
+        # create group with rectangle and text
+        rectangle_group4 = VGroup(rectangle4, rectangle_label4)
+        self.play(rectangle_group4.animate.shift(UP * 2))
+        self.wait(2)
+
+        # control plane cloud-controller-manager component
+        rectangle5 = Rectangle(height=1, width=3, color=WHITE)
+        rectangle5.move_to(rectangle.get_center())
+        self.play(Create(rectangle5))
+        # add text inside the rectangle
+        rectangle_label5 = Text("kube-api-server", font_size=16)
+        rectangle_label5.move_to(rectangle.get_center())
+        self.play(Write(rectangle_label5))
+        self.wait(2)
+
 
         # # Create a rectangle to wrap both of the node groups
         # rectangle = Rectangle(height=2, width=8, color=WHITE)
@@ -165,4 +243,3 @@ class K8sNode(Scene):
         # service_label.next_to(dashed_rectangle, UP)
         # self.play(Write(service_label))
         # self.wait(2)
-
